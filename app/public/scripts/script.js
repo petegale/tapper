62c87a66f9c2925a49d72e959bba21fff1a8dea0
Pete Gale <petegale@gmail.com>
2017-09-30 09:36:55 +0100

    more js fixes

--- app/public/scripts/script.js
+++ app/public/scripts/script.js
@@ -2,11 +2,13 @@ var socket = io();
 
 window.addEventListener("load", function(){
   var createButton = document.getElementById('create');
+  
+  createButton.addEventListener('click', function() {
+    alert("foo");
+    //data can be an object by doing this:
+    //socket.emit("command",{variable,variable});
+    socket.emit("command","data");
+  });
+  
 });
 
-createButton.addEventListener('click', function() {
-  alert("foo");
-  //data can be an object by doing this:
-  //socket.emit("command",{variable,variable});
-  socket.emit("command","data");
-});
